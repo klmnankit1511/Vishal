7b509136bc04f73991bfc1f6d0d5092714b1da9d
klmnankit1511 <klmnankit1511@gmail.com>
2021-06-07 21:07:28 +0530

    Upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCN-mxZPGN6keTPXJ3zwYXX7zcV__xaV90",
+  authDomain: "todo-list-155a3.firebaseapp.com",
+  projectId: "todo-list-155a3",
+  storageBucket: "todo-list-155a3.appspot.com",
+  messagingSenderId: "84383192521",
+  appId: "1:84383192521:web:6b61daab34bb3efb598418",
+  measurementId: "G-HM6CDE7JT3",
+};
